9ed52a2f42bf99cfd14befcb3f58390b5d00ebb6
Sainath Reddy <dukedustine123@gmail.com>
2021-08-28 11:23:40 +0530

    account details

---
+++ Gpay/app/src/main/java/com/masai/sainath/gpay/Settings.kt
@@ -0,0 +1,11 @@
+package com.masai.sainath.gpay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings2)
+    }
+}
