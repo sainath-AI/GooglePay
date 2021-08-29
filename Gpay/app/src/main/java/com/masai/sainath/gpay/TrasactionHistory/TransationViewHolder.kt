7f4a73cd63fcd8aea25ab301ded10f0363bb5182
dudi111 <pd.rajiv.000@gmail.com>
2021-08-29 10:18:40 +0530

    Transationhistory2

---
+++ Gpay/app/src/main/java/com/masai/sainath/gpay/TrasactionHistory/TransationViewHolder.kt
@@ -0,0 +1,21 @@
+package com.masai.sainath.gpay.TrasactionHistory
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.transaction_item_layout.view.*
+
+class TransationViewHolder(val view: View,val listener:clicklisten):RecyclerView.ViewHolder(view) {
+
+    fun setdata(list:TransationModel){
+        view.apply {
+            Glide.with(context).load(list.Image).into(civtran)
+            tvtran.text=list.name
+            tvtran1.text=list.date
+            tvtran2.text=list.amount
+            rlayout.setOnClickListener {
+                listener.onClicked(adapterPosition,list)
+            }
+        }
+    }
+}
